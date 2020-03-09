21ddbc8c83f75599826dc01b37451fbd98e1a2d4
daamian3 <damianchojnacki@op.pl>
2020-03-09 02:53:13 +0100

    Update 2019_10_12_000000_create_users_table.php

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->smallInteger('country_id')->nullable()->default(null);
             $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
