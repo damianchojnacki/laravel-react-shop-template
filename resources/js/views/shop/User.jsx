33c328368486f9b05140eb18ef9ed4808d30dc4c
Damian Chojnacki <daamian3@gmail.com>
2020-03-15 16:26:40 +0100

    User panel links added

--- resources/js/views/shop/User.jsx
+++ resources/js/views/shop/User.jsx
@@ -6,37 +6,44 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons/faShoppingCart
 import './user.scss';
 import { faCog } from "@fortawesome/free-solid-svg-icons/faCog";
 import { faPercentage } from "@fortawesome/free-solid-svg-icons/faPercentage";
+import { InertiaLink } from "@inertiajs/inertia-react";
 
 function User({user}){
     return (
         <Shop>
             <div className="container d-flex flex-wrap justify-content-around">
-                <Card className="user__card m-2 text-center p-0">
-                    <CardBody className="p-5">
-                        <FontAwesomeIcon size="6x" icon={faShoppingCart}/>
-                    </CardBody>
-                    <CardFooter className="h5 px-5 mb-0">
-                       Last orders
-                    </CardFooter>
-                </Card>
+                <InertiaLink href="/user/orders" className="user__card m-2 text-center p-0">
+                    <Card>
+                        <CardBody className="p-5">
+                            <FontAwesomeIcon size="6x" icon={faShoppingCart}/>
+                        </CardBody>
+                        <CardFooter className="h5 px-5 mb-0">
+                           Last orders
+                        </CardFooter>
+                    </Card>
+                </InertiaLink>
 
-                <Card className="user__card m-2 text-center p-0">
-                    <CardBody className="p-5">
-                        <FontAwesomeIcon size="6x" icon={faPercentage}/>
-                    </CardBody>
-                    <CardFooter className="h5 px-5 mb-0">
-                        Your discounts
-                    </CardFooter>
-                </Card>
+                <InertiaLink href="/user/discounts" className="user__card m-2 text-center p-0">
+                    <Card>
+                        <CardBody className="p-5">
+                            <FontAwesomeIcon size="6x" icon={faPercentage}/>
+                        </CardBody>
+                        <CardFooter className="h5 px-5 mb-0">
+                            Your discounts
+                        </CardFooter>
+                    </Card>
+                </InertiaLink>
 
-                <Card className="user__card m-2 text-center p-0">
-                    <CardBody className="p-5">
-                        <FontAwesomeIcon size="6x" icon={faCog}/>
-                    </CardBody>
-                    <CardFooter className="h5 px-5 mb-0">
-                        Settings
-                    </CardFooter>
-                </Card>
+                <InertiaLink href="/user/settings" className="user__card m-2 text-center p-0">
+                    <Card>
+                        <CardBody className="p-5">
+                            <FontAwesomeIcon size="6x" icon={faCog}/>
+                        </CardBody>
+                        <CardFooter className="h5 px-5 mb-0">
+                            Settings
+                        </CardFooter>
+                    </Card>
+                </InertiaLink>
             </div>
         </Shop>
     )
