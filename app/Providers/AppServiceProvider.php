6dbc972d6924595b8ba972b62733a33b181e0f80
Damian Chojnacki <daamian3@gmail.com>
2020-06-24 22:02:00 +0200

    force https (not on local env)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URL::forceScheme('https');
+        App::enviorment() != "local" && URL::forceScheme('https');
     }
 }
